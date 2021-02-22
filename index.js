9bd95a0cd0de5d272f9e6eb036fdc58549261d4c
cfegela <cfegela@cainc.com>
2021-02-22 13:41:23 -0500

    chugging along

--- index.js
+++ index.js
@@ -7,23 +7,22 @@ const app = express();
 app.use(bodyparser.text());
 app.post('/', (req, res) => {
 
-  // write the data from post
   fs.writeFile('postdata.txt', req.body, function (err) {
     if (err) return console.log(err);
   });
 
-  // read in the file
   const readInterface = readline.createInterface({
     input: fs.createReadStream('postdata.txt'),
   });
 
-  // loop over the file and get the fields
   readInterface.on('line', function(line) {
-    console.log(line);
+    if ((line.startsWith('From')) || (line.startsWith('Subject'))) {
+      console.log(line);
+    }
+    //console.log(line);
   });
 
-  // ship it
-  res.send(200);
+  res.sendStatus(200);
 });
 
 app.listen(8080, () => console.log('Started server at http://localhost:8080'));
